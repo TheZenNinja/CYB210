64d89470f5966d7c2154a3b5d0a1340634d5b117
TheZenNinja <65081429+TheZenNinja@users.noreply.github.com>
2021-04-10 19:49:57 -0400

    Final commit to create space on SSD

--- Web Scraping/16.py
+++ Web Scraping/16.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-browser = webdriver.Chrome()
-browser.get("http://inventwithpython.com/")
-link = browser.find_element_by_link_text("Read It Online")
-link.click()
+
+page = "https://google.com/"
+
+browser = webdriver.Edge()
+
+browser.get(page)
+#browser.get("http://inventwithpython.com/")
+#link = browser.find_element_by_link_text("Read It Online")
+#link.click()
 
 #for using keyboard
 #link.sendKeys("Hello World")
--- Web Scraping/8.py
+++ Web Scraping/8.py
@@ -3,9 +3,4 @@ import requests
 
 req = requests.get("http://inventwithpython.com/")
 req.raise_for_status()
-
 soup = bs4.BeautifulSoup(req.text, "html.parser")
-
-main = soup.select("#main")
-
-print(main)
